609d8ca264fedb4818f8ad0b3c37a015d36cfb2a
TanatornBig <tanatorn.nateesanprasert@gmail.com>
2018-11-06 16:17:45 +0700

    install semactic-ui-react and semantic-ui-css to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import firebase from './config/firebase'
 
+import { Button } from 'semantic-ui-react'
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Button primary>Click me bitch !</Button>
 				<h1>Kadraw.io</h1>
 			</div>
 		)
---
+++ src/component/login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Login Form</h1>
+			</React.Fragment>
+		)
+	}
+}
