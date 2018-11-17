f018625eb17c8fbf0b1614df09e5f50dff1683b4
dnaver <dnaver@hotmail.com>
2018-11-17 15:32:59 +0700

    -create game component -create canvas component

---
+++ src/component/game.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Canvas from "./game/canvas.js";
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <Canvas />
+      </div>
+    );
+  }
+}
+
+export default Game;
