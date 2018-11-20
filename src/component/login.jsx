23a94fcc200c80b3ce0ff0ca0c35203de347e93d
DESKTOP-T4S42DV\MaiManju <pinklypop@hotmail.com>
2018-11-20 12:29:39 +0700

    Create Login component, No style

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,13 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+  Grid,
+  Segment,
+  Divider,
+  Input,
+  Button,
+  Image
+} from 'semantic-ui-react';
 
 class Login extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hellow Login</h1>
-			</div>
-		)
-	}
+  state = {
+    imageUrl: 'https://picsum.photos/160'
+  };
+
+  render() {
+    return (
+      <Grid style={{ height: '100vh' }}>
+        <Grid.Row style={{ height: '15%' }} />
+        <Grid.Row style={{ height: '70%' }} centered column={3}>
+          <Grid.Column />
+          <Grid.Column width={8}>
+            <Segment clearing padded="very">
+              <Grid column centered>
+                <Button circular size="mini" icon="arrow left" />
+                <Image src={this.state.imageUrl} circular />
+                <Button circular size="mini" icon="arrow right" />
+              </Grid>
+              <h3>Your Name</h3>
+              <Input fluid placeholder="Guest" />
+              <h3>Join Room</h3>
+              <Input fluid action="Submit" placeholder="00 00 00" />
+              <Divider horizontal>Or</Divider>
+              <Button fluid content="Create Room" secondary />
+            </Segment>
+          </Grid.Column>
+          <Grid.Column />
+        </Grid.Row>
+        <Grid.Row style={{ height: '15%' }} />
+      </Grid>
+    );
+  }
 }
 
-export default Login
+export default Login;
