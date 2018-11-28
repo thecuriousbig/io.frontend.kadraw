4c3596117720ad5315e18718559baef2d80ef664
TanatornBig <tanatorn.nateesanprasert@gmail.com>
2018-11-28 11:46:40 +0700

    fix naja

--- src/component/Room/Lobby/userlist.jsx
+++ src/component/Room/Lobby/userlist.jsx
@@ -1,82 +1,80 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import User from '../user'
-import firebase from "../../../config/firebase";
-import '../../kadraw.css';
-import { Button } from 'semantic-ui-react';
+import firebase from '../../../config/firebase'
+import '../../kadraw.css'
+import { Button } from 'semantic-ui-react'
 
 class UserList extends Component {
-  constructor(props) {
-    super(props);
-    this.lobbyRef = firebase.firestore().collection('Lobby');
-    this.state = {
-      users: []
-    };
-  }
+	constructor(props) {
+		super(props)
+		this.lobbyRef = firebase.firestore().collection('Lobby')
+		this.state = {
+			users: []
+		}
+	}
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props !== prevProps && this.props.users.length > 0) {
-      this.getAllUsersInfo(this.props.users);
-    }
-  }
-  async getAllUsersInfo(users) {
-    const usersWithInfo = await Promise.all(
-      users.map(async user => {
-        const userInfo = await this.getUserInfo(user.id);
-        return { ...userInfo, id: user.id, role: user.role, ready: user.ready };
-      })
-    );
-    this.setState({ users: usersWithInfo });
-  }
-  getUserInfo(userId) {
-    const userRef = firebase.firestore().collection('User');
-    var userData = userRef
-      .doc(userId)
-      .get()
-      .then(doc => {
-        if (!doc.exists) {
-          console.log('No such document!');
-        } else {
-          const { avatar, lobbyId, name, score } = doc.data();
-          return { avatar, lobbyId, name, score };
-        }
-      })
-      .catch(err => {
-        console.log('Error getting document', err);
-      });
-    return userData;
-  }
-  handleClickReady(userId) {
-    this.props.handleReady(userId);
-  }
-  renderUserElement() {
-    if (this.state.users.length > 0) {
-      return this.state.users.map(user => {
-        const readyState = user.ready ? 'ready' : 'not ready';
-        const readyButton = (
-          <Button onClick={this.handleClickReady.bind(this, user.id)}>
-            {user.ready ? 'Unready' : 'Ready'}
-          </Button>
-        );
-        return (
-          <li style={{ padding: '0px' }}>
-            <User user={user} />
-            {user.role === 'Leader' ? null : readyState}
-            {user.role === 'Leader' ? null : readyButton}
-          </li>
-        );
-      });
-    }
-    return 'No user';
-  }
+	componentDidUpdate(prevProps, prevState) {
+		if (this.props !== prevProps && this.props.users.length > 0) {
+			this.getAllUsersInfo(this.props.users)
+		}
+	}
+	async getAllUsersInfo(users) {
+		const usersWithInfo = await Promise.all(
+			users.map(async user => {
+				const userInfo = await this.getUserInfo(user.id)
+				return { ...userInfo, id: user.id, role: user.role, ready: user.ready }
+			})
+		)
+		this.setState({ users: usersWithInfo })
+	}
+	getUserInfo(userId) {
+		const userRef = firebase.firestore().collection('User')
+		var userData = userRef
+			.doc(userId)
+			.get()
+			.then(doc => {
+				if (!doc.exists) {
+					console.log('No such document!')
+				} else {
+					const { avatar, lobbyId, name, score } = doc.data()
+					return { avatar, lobbyId, name, score }
+				}
+			})
+			.catch(err => {
+				console.log('Error getting document', err)
+			})
+		return userData
+	}
+	handleClickReady(userId) {
+		this.props.handleReady(userId)
+	}
+	renderUserElement() {
+		if (this.state.users.length > 0) {
+			return this.state.users.map((user, index) => {
+				const readyState = user.ready ? 'ready' : 'not ready'
+				const readyButton = (
+					<Button onClick={this.handleClickReady.bind(this, user.id)}>{user.ready ? 'Unready' : 'Ready'}</Button>
+				)
+				return (
+					<li style={{ padding: '0px' }}>
+						<User user={user} key={index} />
+						{user.role === 'Leader' ? null : readyState}
+						{user.role === 'Leader' ? null : readyButton}
+					</li>
+				)
+			})
+		}
+		return 'No user'
+	}
 
-  render() {
-    let users_ele = this.renderUserElement();
-    return (
-      <div>
-        <ul>{users_ele}</ul>
-      </div>
-    );
-  }
+	render() {
+		let users_ele = this.renderUserElement()
+		return (
+			<div>
+				<ul>{users_ele}</ul>
+			</div>
+		)
+	}
 }
 
-export default UserList;
+export default UserList
--- src/component/Room/user.jsx
+++ src/component/Room/user.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import { Comment } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import { Comment } from 'semantic-ui-react'
 class User extends Component {
-  constructor(props) {
-    super(props);
-  }
-  renderUserElement() {
-    if (this.props.user) {
-      return (
-        <Comment.Group>
-          <Comment style={{ textAlign: 'left' }}>
-            <Comment.Avatar src={this.props.user.avatar} />
-            <Comment.Content>
-              <Comment.Author style={{ color: 'black', fontSize: '18px' }}>
-                {this.props.user.name}
-              </Comment.Author>
-            </Comment.Content>
-          </Comment>
-        </Comment.Group>
-      );
-    }
-    return;
-  }
-  render() {
-    let user_ele = this.renderUserElement();
-    return <div>{user_ele}</div>;
-  }
+	constructor(props) {
+		super(props)
+	}
+	renderUserElement() {
+		if (this.props.user) {
+			return (
+				<Comment.Group>
+					<Comment style={{ textAlign: 'left' }}>
+						<Comment.Avatar src={this.props.user.avatar} />
+						<Comment.Content>
+							<Comment.Author style={{ color: 'black', fontSize: '18px' }}>{this.props.user.name}</Comment.Author>
+						</Comment.Content>
+					</Comment>
+				</Comment.Group>
+			)
+		}
+		return
+	}
+	render() {
+		let user_ele = this.renderUserElement()
+		return <div>{user_ele}</div>
+	}
 }
 
-export default User;
+export default User
