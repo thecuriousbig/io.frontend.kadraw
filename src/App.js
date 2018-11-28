62cd3dcc1599a11275a2430c74290bc9863704ee
PatiponICE <darkicei30y@hotmail.com>
2018-11-28 14:55:44 +0700

    create wordselector modal

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom'
 
 import Home from './component/home'
 import Lobby from './component/lobby'
+import WordSelector from './component/wordselector'
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Route exact path="/" component={Home} />
 				<Route exact path="/lobby/:lobbyId" component={Lobby} />
+				<Route exact path="/word" component={WordSelector} />
 			</div>
 		)
 	}
