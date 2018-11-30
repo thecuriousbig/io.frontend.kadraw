96129e847bfd1a6b87ac02f5bc26eacfb93e7a05
Manchuporn <pinklypop@hotmail.com>
2018-11-30 20:53:03 +0700

    Style play room

--- src/component/Room/Play/Game/toolbox.jsx
+++ src/component/Room/Play/Game/toolbox.jsx
@@ -1,58 +1,89 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { GithubPicker } from 'react-color';
+import { Button, Icon, Grid } from 'semantic-ui-react';
 class Game extends Component {
   constructor(props) {
     super(props);
   }
-  colors_list = {
-    black: "#000000", //black
-    red: "#FF0000", //red
-    green: "#00FF00", //green
-    blue: "#0000FF" ///blue
+  state = {
+    color: 'grey',
+    isPickerVisible: false
   };
-  brush_size_lists = [1, 2, 5, 10];
-  handleClickColor(color_code) {
-    this.props.changeBrushColor(color_code);
-  }
+
+  // brush_size_lists = [1, 2, 5, 10];
+
+  handleClickColor = color => {
+    this.setState({ color: color.hex });
+    this.props.changeBrushColor(this.state.color);
+    this.props.changeBrushSize('2');
+  };
+
   handleClickSize(size) {
     this.props.changeBrushSize(size);
   }
-  handleClickEraser() {
-    this.props.changeBrushColor("#FFFFFF");
-    this.props.changeBrushCap("round");
-  }
-  handleClickClear() {
+  handleClickEraser = () => {
+    this.props.changeBrushColor('#FFFFFF');
+    this.props.changeBrushSize('3');
+  };
+  handleClickClear = () => {
     this.props.handleClearCanvas();
-  }
+  };
+
+  renderPickColorButton = event => {
+    let a = !this.state.isPickerVisible;
+    console.log('aaaaa ', this.state.isPickerVisible, a);
+    this.setState({ isPickerVisible: a });
+  };
+
   render() {
-    var color_button = Object.keys(this.colors_list).map(color => (
-      <button
-        onClick={this.handleClickColor.bind(this, this.colors_list[color])}
-      >
-        {color}
-      </button>
-    ));
-    var size_button = this.brush_size_lists.map(size => (
-      <div>
-        <button onClick={this.handleClickSize.bind(this, size)}>
-          + {size}
-        </button>
-        <button onClick={this.handleClickSize.bind(this, -1 * size)}>
-          - {size}
-        </button>
-      </div>
-    ));
     var eraser_button = (
-      <button onClick={this.handleClickEraser.bind(this)}>Eraser</button>
+      <Button basic onClick={this.handleClickEraser} icon>
+        <Icon name="eraser" />
+      </Button>
     );
     var reset_button = (
-      <button onClick={this.handleClickClear.bind(this)}>Clear</button>
+      <Button basic icon onClick={this.handleClickClear}>
+        <Icon name="trash" />
+      </Button>
     );
+
     return (
       <div>
-        {color_button} {size_button} {reset_button} {eraser_button}
+        <Grid textAlign="center" style={{ height: '150px' }}>
+          <Grid.Column>{reset_button}</Grid.Column>
+          <Grid.Column>{eraser_button}</Grid.Column>
+          <Grid.Column>
+            <Grid.Row>
+              <Button basic icon onClick={this.renderPickColorButton}>
+                <Icon name="tint" style={{ color: this.state.color }} />
+              </Button>
+            </Grid.Row>
+            <Grid.Row>
+              {this.state.isPickerVisible ? (
+                <GithubPicker
+                  color={this.state.color}
+                  onChangeComplete={this.handleClickColor}
+                />
+              ) : (
+                ''
+              )}
+            </Grid.Row>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
 }
 
 export default Game;
+
+// var size_button = this.brush_size_lists.map(size => (
+//   <div>
+//   <button onClick={this.handleClickSize.bind(this, size)}>
+//     + {size}
+//   </button>
+//   <button onClick={this.handleClickSize.bind(this, -1 * size)}>
+//     - {size}
+//   </button>
+// </div>
+// ));
--- src/component/Room/Play/userlist.jsx
+++ src/component/Room/Play/userlist.jsx
@@ -1,70 +1,77 @@
-import React, { Component } from "react";
-import User from '../user'
-import firebase from "../../../config/firebase";
+import React, { Component } from 'react';
+import User from '../user';
+import firebase from '../../../config/firebase';
+
+import { Button, Comment, Grid } from 'semantic-ui-react';
 class UserList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props !== prevProps && this.props.users.length > 0) {
+      this.getAllUsersInfo(this.props.users);
+    }
+  }
+  async getAllUsersInfo(users) {
+    const usersWithInfo = await Promise.all(
+      users.map(async user => {
+        const userInfo = await this.getUserInfo(user.id);
+        return {
+          ...userInfo,
+          id: user.id,
+          role: user.role,
+          gameRole: user.gameRole,
+          gameScore: user.score
         };
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if ((this.props !== prevProps) && this.props.users.length > 0) {
-            this.getAllUsersInfo(this.props.users);
+      })
+    );
+    this.setState({ users: usersWithInfo });
+  }
+  getUserInfo(userId) {
+    const userRef = firebase.firestore().collection('User');
+    var userData = userRef
+      .doc(userId)
+      .get()
+      .then(doc => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          const { avatar, lobbyId, name, score } = doc.data();
+          return { avatar, lobbyId, name, score };
         }
-    }
-    async getAllUsersInfo(users) {
-        const usersWithInfo = await Promise.all(
-            users.map(async user => {
-                const userInfo = await this.getUserInfo(user.id)
-                return { ...userInfo, id: user.id, role: user.role, gameRole: user.gameRole, gameScore: user.score };
-            })
-        )
-        this.setState({ users: usersWithInfo });
-    }
-    getUserInfo(userId) {
-        const userRef = firebase.firestore().collection("User");
-        var userData = userRef
-            .doc(userId)
-            .get()
-            .then((doc) => {
-                if (!doc.exists) {
-                    console.log("No such document!");
-                } else {
-                    const { avatar, lobbyId, name, score } = doc.data()
-                    return { avatar, lobbyId, name, score }
-                }
-            })
-            .catch(err => {
-                console.log("Error getting document", err);
-            });
-        return userData;
-    }
-    renderUserElement() {
-        if (this.state.users.length > 0) {
-            return this.state.users.map(user => {
-                return (
-                    <li>
-                        <User user={user} />
-                        <strong>Score : {user.gameScore}</strong>
-                        <br />
-                        <strong>{user.gameRole}</strong>
-                    </li>
-                )
-            });
-        }
-        return 'No user'
-    }
-
-    render() {
-        let users_ele = this.renderUserElement();
+      })
+      .catch(err => {
+        console.log('Error getting document', err);
+      });
+    return userData;
+  }
+  renderUserElement() {
+    if (this.state.users.length > 0) {
+      return this.state.users.map(user => {
         return (
-            <div>
-                <ul>{users_ele}</ul>
-            </div >
+          <Comment.Group size="Small">
+            <User user={user} />
+            <strong>Score : {user.gameScore}</strong>
+            <strong>{user.gameRole}</strong>
+          </Comment.Group>
         );
+      });
     }
+    return 'No user';
+  }
+
+  render() {
+    let users_ele = this.renderUserElement();
+    return (
+      <div>
+        <ul>{users_ele}</ul>
+      </div>
+    );
+  }
 }
 
 export default UserList;
